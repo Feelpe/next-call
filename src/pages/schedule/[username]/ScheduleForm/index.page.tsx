64cb11d921597c22a51447be7d4aeb8f7fa7f6fc
felipe <felipe.silva.santos.lawliet1@gmail.com>
2023-07-24 13:15:55 -0300

    feat: add confirmation form, calendar mounth navigation and calc weeks and days

--- src/pages/schedule/[username]/ScheduleForm/index.page.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.page.tsx
@@ -2,5 +2,5 @@ import { CalendarStep } from './CalendarStep/index.page'
 import { ConfirmStep } from './ConfirmStep/index.page'
 
 export function ScheduleForm() {
-  return <ConfirmStep />
+  return <CalendarStep />
 }
